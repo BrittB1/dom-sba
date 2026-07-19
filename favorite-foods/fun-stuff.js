d1beb8eabba08c311d9fa195c7c45b1167d9b101
Brittany Buchanan <157894600+BrittB1@users.noreply.github.com>
2026-07-19 19:42:40 -0400

    Restructured project into single folder

---
+++ favorite-foods/fun-stuff.js
@@ -0,0 +1,12 @@
+const foods = [
+    {
+        name: "Tirimisu",
+        image: "./images/tirimisu.jpg",
+        description: "an Italian dessert made with coffee-soaked ladyfingers covered with a cream of egg yolks, sugar, mascarpone, and cocoa powder. It originated in northeastern Italy, and modern versions were popularized in restaurants from the late 1960s."
+    },
+    {
+        name: "Curried Goat",
+        image: "./images/curried-goat.jpg",
+        description: "a curry dish prepared with goat meat, originating from the Indian subcontinent. The dish is a staple in Southeast Asian cuisine, Caribbean cuisine, and the cuisine of the Indian subcontinent."
+    }
+];
